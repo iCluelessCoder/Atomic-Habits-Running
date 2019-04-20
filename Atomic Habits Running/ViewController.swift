bcbc2df1f21e76a51791258975bffcd4d1335a9d
Marek Justus <icluelesscoder@gmail.com>
2019-04-20 09:25:42 +0100

    Blank version.

---
+++ Atomic Habits Running/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Atomic Habits Running
+//
+//  Created by Marek Justus on 20/04/2019.
+//  Copyright © 2019 Marek Justus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
