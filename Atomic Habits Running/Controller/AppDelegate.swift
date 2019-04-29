0dc6bfd9c309f4ec647b48873940af869d2be719
Marek Justus <icluelesscoder@gmail.com>
2019-04-29 16:29:57 +0100

    Added menu button and re-structurised Mainstoryboard.

--- Atomic Habits Running/Controller/AppDelegate.swift
+++ Atomic Habits Running/Controller/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UITabBar.appearance().barTintColor = .darkGray
         UITabBar.appearance().tintColor = .white
@@ -89,6 +88,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
 }
 
--- Atomic Habits Running/Controller/ViewController.swift
+++ Atomic Habits Running/Controller/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
     @IBOutlet weak var runningMainCard: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        sideMenus()
+        customizeNavBar()
         
     runningMainCard.setImage(UIImage(named: "main-card.png"), for: UIControl.State.normal)
         
@@ -36,6 +42,33 @@ class ViewController: UIViewController {
             }
             
     }
+    
+    func sideMenus() {
+        
+        if revealViewController() != nil {
+            
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            revealViewController().rightViewRevealWidth = 160
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+        }
+        
+        
+    }
 
+    func customizeNavBar() {
+        
+        navigationController?.navigationBar.tintColor = UIColor(displayP3Red: 255/255, green: 256/255, blue: 256/255, alpha: 1)
+        navigationController?.navigationBar.barTintColor = UIColor(displayP3Red: 255/255, green: 87/255, blue: 35/255, alpha: 1)
+        
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+    }
+    
+    
+    
 }
 
